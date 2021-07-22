8e01cce0a88776590bcb001fb26dc4de30764599
dinhhuychuong3005 <dinhhuychuong3005@gmail.com>
2021-07-23 01:57:07 +0700

    Custom Validation

---
+++ src/main/java/com/codegym/controller/PhoneController.java
@@ -0,0 +1,38 @@
+package com.codegym.controller;
+
+import com.codegym.model.PhoneNumber;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+@Controller
+public class PhoneController {
+    @GetMapping("/form")
+    public ModelAndView showForm(){
+      ModelAndView modelAndView = new ModelAndView("/index");
+      modelAndView.addObject("phoneNumber",new PhoneNumber());
+      return modelAndView;
+    }
+    @PostMapping("/result")
+    public ModelAndView CheckValidation(@Valid @ModelAttribute("phoneNumber")PhoneNumber phoneNumber, BindingResult bindingResult){
+        new PhoneNumber().validate(phoneNumber,bindingResult);
+        if ((bindingResult.hasFieldErrors())){
+            ModelAndView modelAndView = new ModelAndView("/index");
+
+            return modelAndView;
+        }
+        else {
+//            model.addAttribute("phoneNumber", phoneNumber);
+//            return "/result";
+            ModelAndView modelAndView = new ModelAndView("/result");
+            modelAndView.addObject("phoneNumber",phoneNumber);
+            return modelAndView;
+        }
+    }
+}
